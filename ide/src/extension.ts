0582b03fae60c9109260569166291b6c0d21336c
Xavier Denis <xldenis@gmail.com>
2023-03-16 10:48:35 +0100

    Add eslint

--- ide/src/extension.ts
+++ ide/src/extension.ts
@@ -3,18 +3,18 @@ import { workspace, ExtensionContext } from "vscode";
 import * as vscode from "vscode";
 
 import {
-  DidChangeTextDocumentNotification,
-  DidCloseTextDocumentNotification,
-  DidOpenTextDocumentNotification,
-  DocumentUri,
-  LanguageClient,
-  LanguageClientOptions,
-  ProtocolNotificationType,
-  ProtocolRequestType,
-  RequestType,
-  ServerOptions,
-  TransportKind,
-  URI,
+    DidChangeTextDocumentNotification,
+    DidCloseTextDocumentNotification,
+    DidOpenTextDocumentNotification,
+    DocumentUri,
+    LanguageClient,
+    LanguageClientOptions,
+    ProtocolNotificationType,
+    ProtocolRequestType,
+    RequestType,
+    ServerOptions,
+    TransportKind,
+    URI,
 } from "vscode-languageclient/node";
 import { createConverter } from "vscode-languageclient/lib/common/codeConverter";
 import { countReset } from "console";
@@ -31,209 +31,209 @@ type ResolveSessionResponse = ResolveSessionParams;
 namespace ResolveSession {
   export const method: "proof/resolveSession" = "proof/resolveSession";
   export const type: RequestType<ResolveSessionParams, ResolveSessionResponse | null, {}> = new RequestType(
-    method,
-    undefined
+      method,
+      undefined
   );
 }
 
-let trees: Map<string, TaskTree> = new Map();
+const trees: Map<string, TaskTree> = new Map();
 let proofDocs: Set<vscode.Uri>;
 
 function buildCommands(): [string, (...args: any[]) => any][] {
-  return [
-    [
-      "taskTree.run_auto_0",
-      (task: TaskNode) => {
-        vscode.commands.executeCommand("why3.run_transformation", task.uri, task.id, "Auto_level_0");
-      },
-    ],
-    [
-      "taskTree.split_vc",
-      (task: TaskNode) => {
-        vscode.commands.executeCommand("why3.run_transformation", task.uri, task.id, "Split_VC");
-      },
-    ],
-    [
-      "extension.reset_session",
-      () => {
-        let uri: DocumentUri = vscode.window.activeTextEditor?.document.uri?.toString()!;
-        client.sendRequest("proof/resetSession", { uri: uri, dummy: true });
-        vscode.window.showInformationMessage("Session Reset");
-      },
-    ],
-    [
-      "extension.reload_session",
-      () => {
-        let uri: DocumentUri = vscode.window.activeTextEditor?.document.uri?.toString()!;
-        console.log(uri);
-        client.sendNotification("proof/reloadSession", {
-          uri: uri,
-          dummy: true,
-        });
+    return [
+        [
+            "taskTree.run_auto_0",
+            (task: TaskNode) => {
+                vscode.commands.executeCommand("why3.run_transformation", task.uri, task.id, "Auto_level_0");
+            },
+        ],
+        [
+            "taskTree.split_vc",
+            (task: TaskNode) => {
+                vscode.commands.executeCommand("why3.run_transformation", task.uri, task.id, "Split_VC");
+            },
+        ],
+        [
+            "extension.reset_session",
+            () => {
+                const uri: DocumentUri = vscode.window.activeTextEditor?.document.uri?.toString()!;
+                client.sendRequest("proof/resetSession", { uri: uri, dummy: true });
+                vscode.window.showInformationMessage("Session Reset");
+            },
+        ],
+        [
+            "extension.reload_session",
+            () => {
+                const uri: DocumentUri = vscode.window.activeTextEditor?.document.uri?.toString()!;
+                console.log(uri);
+                client.sendNotification("proof/reloadSession", {
+                    uri: uri,
+                    dummy: true,
+                });
 
-        vscode.window.showInformationMessage("Session Reloaded");
-      },
-    ],
-    [
-      "why3.run_transformation",
-      (uri: DocumentUri, node: number, command: string) => {
-        client.sendRequest("proof/runTransformation", {
-          uri: uri,
-          node: node,
-          command: command,
-        });
-      },
-    ],
-    // ['proof/changeTreeNode', params => {
-    // 	let node = trees.get(params.uri)!.getChild(params.id);
-    // 	console.log("update node ", params);
-    // 	if (params.info[0] == 'Proved' && node != undefined) {
-    // 		node.proved = params.info[1];
-    // 	};
+                vscode.window.showInformationMessage("Session Reloaded");
+            },
+        ],
+        [
+            "why3.run_transformation",
+            (uri: DocumentUri, node: number, command: string) => {
+                client.sendRequest("proof/runTransformation", {
+                    uri: uri,
+                    node: node,
+                    command: command,
+                });
+            },
+        ],
+        // ['proof/changeTreeNode', params => {
+        // 	let node = trees.get(params.uri)!.getChild(params.id);
+        // 	console.log("update node ", params);
+        // 	if (params.info[0] == 'Proved' && node != undefined) {
+        // 		node.proved = params.info[1];
+        // 	};
+        // 	treeDataProvider.refresh();
+        // }],
+        // ['proof/removeTreeNode', params => {
+        // 	console.log("remove node", params);
+        // 	if (!trees.has(params.uri)) {
+        // 		trees.set(params.uri, new TaskTree());
+        // 	};
+
+        // 	trees.get(params.uri)!.remove(params.id);
+        // 	treeDataProvider.refresh();
+
+        // }],
+        // ['proof/addTreeNode', params => {
+        // 	let node = new GoalNode(params.uri, params.id, params.parent_id, params.name, false, []);
+        // 	if (!trees.has(params.uri)) {
+        // 		trees.set(params.uri, new TaskTree());
+        // 	};
+
+        // 	trees.get(params.uri)!.insertChild(params.parent_id, node);
+        // 	console.log("create node ", params);
+        // 	view.reveal(node);
+
     // 	treeDataProvider.refresh();
     // }],
-    // ['proof/removeTreeNode', params => {
-    // 	console.log("remove node", params);
-    // 	if (!trees.has(params.uri)) {
-    // 		trees.set(params.uri, new TaskTree());
-    // 	};
-
-    // 	trees.get(params.uri)!.remove(params.id);
-    // 	treeDataProvider.refresh();
-
-    // }],
-    // ['proof/addTreeNode', params => {
-    // 	let node = new GoalNode(params.uri, params.id, params.parent_id, params.name, false, []);
-    // 	if (!trees.has(params.uri)) {
-    // 		trees.set(params.uri, new TaskTree());
-    // 	};
-
-    // 	trees.get(params.uri)!.insertChild(params.parent_id, node);
-    // 	console.log("create node ", params);
-    // 	view.reveal(node);
-
-    // 	treeDataProvider.refresh();
-    // }],
-  ];
+    ];
 }
 
 async function startServer(): Promise<LanguageClient> {
-  let serverSetting: string | undefined = vscode.workspace.getConfiguration("whycode").get("executablePath");
-  if (serverSetting == undefined || serverSetting == "") {
-    serverSetting = "whycode";
-  }
-  const serverModule: string = serverSetting!;
-  const serverArgs: string[] = vscode.workspace.getConfiguration("whycode").get("extraArgs")!;
+    let serverSetting: string | undefined = vscode.workspace.getConfiguration("whycode").get("executablePath");
+    if (serverSetting == undefined || serverSetting == "") {
+        serverSetting = "whycode";
+    }
+    const serverModule: string = serverSetting!;
+    const serverArgs: string[] = vscode.workspace.getConfiguration("whycode").get("extraArgs")!;
 
-  // The debug options for the server
-  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-  const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+    // The debug options for the server
+    // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+    const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
 
-  // If the extension is launched in debug mode then the debug server options are used
-  // Otherwise the run options are used
-  const serverOptions: ServerOptions = {
-    run: {
-      command: serverModule,
-      args: serverArgs,
-    },
-    debug: {
-      command: serverModule,
-      args: serverArgs,
-      // options: debugOptions
-    },
-  };
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+    const serverOptions: ServerOptions = {
+        run: {
+            command: serverModule,
+            args: serverArgs,
+        },
+        debug: {
+            command: serverModule,
+            args: serverArgs,
+            // options: debugOptions
+        },
+    };
 
-  // Options to control the language client
-  const clientOptions: LanguageClientOptions = {
-    documentSelector: [],
-    synchronize: {},
-  };
+    // Options to control the language client
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [],
+        synchronize: {},
+    };
 
-  // Create the language client and start the client.
-  client = new LanguageClient("whycode", "Why3", serverOptions, clientOptions);
+    // Create the language client and start the client.
+    client = new LanguageClient("whycode", "Why3", serverOptions, clientOptions);
 
-  await client.start();
-  return client;
+    await client.start();
+    return client;
 }
 
 function setupServerEvents() {
-  workspace.onDidChangeTextDocument((event) => {
-    if (event.contentChanges.length === 0) {
-      return;
-    }
-    // check if the document which changed is one we care about
-    // if so, forward that to the LSP server.
-    if (proofDocs.has(event.document.uri)) {
-      // This seems to only be for 'incremental sync'
-      client.sendNotification(
-        DidChangeTextDocumentNotification.type,
-        createConverter().asChangeTextDocumentParams(event)
-      );
-    }
-  });
+    workspace.onDidChangeTextDocument((event) => {
+        if (event.contentChanges.length === 0) {
+            return;
+        }
+        // check if the document which changed is one we care about
+        // if so, forward that to the LSP server.
+        if (proofDocs.has(event.document.uri)) {
+            // This seems to only be for 'incremental sync'
+            client.sendNotification(
+                DidChangeTextDocumentNotification.type,
+                createConverter().asChangeTextDocumentParams(event)
+            );
+        }
+    });
 
-  workspace.onDidCloseTextDocument((e) => {
+    workspace.onDidCloseTextDocument((e) => {
     // vscode.window.showInformationMessage("Document Close!");
     // If the document is in our list, then remove it.
-    proofDocs.delete(e.uri);
-    // And notify the server
-    client.sendNotification(DidCloseTextDocumentNotification.type, createConverter().asCloseTextDocumentParams(e));
-  });
+        proofDocs.delete(e.uri);
+        // And notify the server
+        client.sendNotification(DidCloseTextDocumentNotification.type, createConverter().asCloseTextDocumentParams(e));
+    });
 }
 
 function setupTaskTree(context: ExtensionContext) {
-  const treeDataProvider = new TaskDataProvider(new TaskTree());
+    const treeDataProvider = new TaskDataProvider(new TaskTree());
 
-  const view: vscode.TreeView<TaskNode> = vscode.window.createTreeView("taskTree", {
-    treeDataProvider,
-  });
-  context.subscriptions.push(view);
+    const view: vscode.TreeView<TaskNode> = vscode.window.createTreeView("taskTree", {
+        treeDataProvider,
+    });
+    context.subscriptions.push(view);
 
-  let focusOnTree = function (id: string) {
-    treeDataProvider.tree = trees.get(id)!;
-    treeDataProvider.refresh();
-  };
+    const focusOnTree = function (id: string) {
+        treeDataProvider.tree = trees.get(id)!;
+        treeDataProvider.refresh();
+    };
 
-  setTimeout(async function () {
-    let uri: DocumentUri = vscode.window.activeTextEditor?.document.uri?.toString()!;
+    setTimeout(async function () {
+        const uri: DocumentUri = vscode.window.activeTextEditor?.document.uri?.toString()!;
 
-    let id = await client.sendRequest(ResolveSession.type, { uri: uri });
-    if (id != undefined && trees.get(id.uri) != undefined) {
-      focusOnTree(id.uri);
-    }
-  }, 250);
+        const id = await client.sendRequest(ResolveSession.type, { uri: uri });
+        if (id != undefined && trees.get(id.uri) != undefined) {
+            focusOnTree(id.uri);
+        }
+    }, 250);
 
-  let disposable = vscode.window.onDidChangeActiveTextEditor(async (e) => {
-    if (e != undefined) {
-      let id = await client.sendRequest(ResolveSession.type, {
-        uri: e.document.uri.toString(),
-      });
-      if (id != undefined && trees.get(id.uri) != undefined) {
-        focusOnTree(id.uri);
-      }
-    }
-  });
+    const disposable = vscode.window.onDidChangeActiveTextEditor(async (e) => {
+        if (e != undefined) {
+            const id = await client.sendRequest(ResolveSession.type, {
+                uri: e.document.uri.toString(),
+            });
+            if (id != undefined && trees.get(id.uri) != undefined) {
+                focusOnTree(id.uri);
+            }
+        }
+    });
 
-  context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 export async function activate(context: ExtensionContext) {
-  var proofDocs: Set<vscode.Uri> = new Set();
+    const proofDocs: Set<vscode.Uri> = new Set();
 
-  let client = await startServer();
-  setupServerEvents();
-  setupTaskTree(context);
+    const client = await startServer();
+    setupServerEvents();
+    setupTaskTree(context);
 
-  buildCommands().forEach(([name, command]) => {
-    let disposable = vscode.commands.registerCommand(name, command);
+    buildCommands().forEach(([name, command]) => {
+        const disposable = vscode.commands.registerCommand(name, command);
 
-    context.subscriptions.push(disposable);
-  });
+        context.subscriptions.push(disposable);
+    });
 }
 
 export function deactivate(): Thenable<void> | undefined {
-  if (!client) {
-    return undefined;
-  }
-  return client.stop();
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
 }
--- ide/src/tree.ts
+++ ide/src/tree.ts
@@ -5,9 +5,9 @@ abstract class ProofStatus {
 }
 
 class Unproved implements ProofStatus {
-  statusIcon(): vscode.ThemeIcon {
-    return new vscode.ThemeIcon("question", new vscode.ThemeColor("list.warningForeground"));
-  }
+    statusIcon(): vscode.ThemeIcon {
+        return new vscode.ThemeIcon("question", new vscode.ThemeColor("list.warningForeground"));
+    }
 }
 
 // Provide access to document URI
@@ -34,27 +34,27 @@ export class GoalNode extends TaskNode {}
 
 type NewProofAttemptNode = NewNodeNotif<NodeType.NProofAttempt>;
 class ProofAttemptNode extends TaskNode {
-  constructor(
+    constructor(
     readonly uri: vscode.Uri,
     readonly id: NodeId,
     readonly parentId: NodeId,
     readonly name: string,
     readonly detached: boolean,
     public proofStatus?: AttemptStatus
-  ) {
-    super(uri, id, parentId, name, detached, []);
-  }
+    ) {
+        super(uri, id, parentId, name, detached, []);
+    }
 
-  // get proved(): boolean {
-  //   if (this.proofStatus == undefined) {
-  //     return false;
-  //   }
+    // get proved(): boolean {
+    //   if (this.proofStatus == undefined) {
+    //     return false;
+    //   }
 
-  //   if (this.proofStatus.type == "Done") {
-  //     return true;
-  //   }
-  //   return true;
-  // }
+    //   if (this.proofStatus.type == "Done") {
+    //     return true;
+    //   }
+    //   return true;
+    // }
 }
 
 type NewTransformationNode = NewNodeNotif<NodeType.NTransformation>;
@@ -65,103 +65,103 @@ export class TaskTree {
   private nodeMap: Map<NodeId, TaskNode> = new Map();
 
   insertChild(parent: NodeId, child: TaskNode) {
-    const parentNode = this.nodeMap.get(parent);
-    this.nodeMap.set(child.id, child);
-    if (parent != 0) {
-      let child_ix = parentNode!.children.findIndex((n) => n.id == child.id);
-      if (child_ix == -1) {
+      const parentNode = this.nodeMap.get(parent);
+      this.nodeMap.set(child.id, child);
+      if (parent != 0) {
+          const child_ix = parentNode!.children.findIndex((n) => n.id == child.id);
+          if (child_ix == -1) {
         parentNode!.children.push(child);
-      } else {
+          } else {
         // should probably just error here on the long term.
         parentNode!.children[child_ix] = child; // handle children of the old child
-      }
-    } else {
-      let old_ix = this.roots.findIndex((n) => n.id == child.id);
-      if (old_ix == -1) {
-        this.roots.push(child);
+          }
       } else {
-        this.roots[old_ix] = child;
+          const old_ix = this.roots.findIndex((n) => n.id == child.id);
+          if (old_ix == -1) {
+              this.roots.push(child);
+          } else {
+              this.roots[old_ix] = child;
+          }
       }
-    }
   }
 
   remove(id: NodeId) {
-    if (id == 0) {
-      this.nodeMap.clear();
-      this.roots = [];
-      return;
-    }
-    let node = this.nodeMap.get(id);
-    let parent = this.nodeMap.get(node!.parentId)!;
+      if (id == 0) {
+          this.nodeMap.clear();
+          this.roots = [];
+          return;
+      }
+      const node = this.nodeMap.get(id);
+      const parent = this.nodeMap.get(node!.parentId)!;
 
-    let parent_ix = parent.children.findIndex((n) => n == n);
-    parent.children.splice(parent_ix, 1);
+      const parent_ix = parent.children.findIndex((n) => n == n);
+      parent.children.splice(parent_ix, 1);
 
-    for (const child of node?.children || []) {
-      this.remove(child.id);
-    }
+      for (const child of node?.children || []) {
+          this.remove(child.id);
+      }
 
-    this.nodeMap.delete(id);
+      this.nodeMap.delete(id);
   }
 
   getChild(id: NodeId): TaskNode | undefined {
-    return this.nodeMap.get(id);
+      return this.nodeMap.get(id);
   }
 }
 
 export class TaskDataProvider implements vscode.TreeDataProvider<TaskNode> {
-  constructor(public tree: TaskTree) {}
+    constructor(public tree: TaskTree) {}
 
   // Refreshing the tree view
   private _onDidChangeTreeData: vscode.EventEmitter<TaskNode | undefined | void> = new vscode.EventEmitter<any>();
   readonly onDidChangeTreeData: vscode.Event<any> = this._onDidChangeTreeData.event;
 
   public refresh(): any {
-    this._onDidChangeTreeData.fire();
+      this._onDidChangeTreeData.fire();
   }
 
   public getTreeItem(element: TaskNode): vscode.TreeItem {
-    var icon = new vscode.ThemeIcon("question", new vscode.ThemeColor("notebookStatusRunningIcon.foreground"));
+      let icon = new vscode.ThemeIcon("question", new vscode.ThemeColor("notebookStatusRunningIcon.foreground"));
 
-    if (element.proved == true) {
-      icon = new vscode.ThemeIcon(
-        "notebook-state-success",
-        new vscode.ThemeColor("notebookStatusSuccessIcon.foreground")
-      );
-    } else if (element.proved == false) {
-      icon = new vscode.ThemeIcon("notebook-state-error", new vscode.ThemeColor("notebookStatusErrorIcon.foreground"));
-    }
-
-    let state: vscode.TreeItemCollapsibleState;
-
-    if (element.children.length > 0) {
-      if (element.proved) {
-        state = vscode.TreeItemCollapsibleState.Collapsed;
-      } else {
-        state = vscode.TreeItemCollapsibleState.Expanded;
+      if (element.proved == true) {
+          icon = new vscode.ThemeIcon(
+              "notebook-state-success",
+              new vscode.ThemeColor("notebookStatusSuccessIcon.foreground")
+          );
+      } else if (element.proved == false) {
+          icon = new vscode.ThemeIcon("notebook-state-error", new vscode.ThemeColor("notebookStatusErrorIcon.foreground"));
       }
-    } else {
-      state = vscode.TreeItemCollapsibleState.None;
-    }
 
-    return {
-      label: element.name,
-      id: element.id.toString(),
-      collapsibleState: state,
-      iconPath: icon,
-    };
+      let state: vscode.TreeItemCollapsibleState;
+
+      if (element.children.length > 0) {
+          if (element.proved) {
+              state = vscode.TreeItemCollapsibleState.Collapsed;
+          } else {
+              state = vscode.TreeItemCollapsibleState.Expanded;
+          }
+      } else {
+          state = vscode.TreeItemCollapsibleState.None;
+      }
+
+      return {
+          label: element.name,
+          id: element.id.toString(),
+          collapsibleState: state,
+          iconPath: icon,
+      };
   }
 
   getChildren(element?: TaskNode): TaskNode[] {
-    if (element == undefined) {
-      return this.tree.roots;
-    } else {
-      return Array.from(element?.children);
-    }
+      if (element == undefined) {
+          return this.tree.roots;
+      } else {
+          return Array.from(element?.children);
+      }
   }
 
   getParent(element: TaskNode): TaskNode {
-    return this.tree.getChild(element.parentId)!;
+      return this.tree.getChild(element.parentId)!;
   }
 }
 type Prover = undefined;
@@ -192,17 +192,17 @@ type AttemptStatusMessage =
   | ["Removed", Prover];
 
 function fromAttemptMessage(msg: AttemptStatusMessage): AttemptStatus {
-  if (msg[0] == "Done") {
-    return { type: msg[0], result: msg[1] };
-  } else if (msg[0] == "Uninstalled") {
-    return { type: msg[0], prover: msg[1] };
-  } else if (msg[0] == "UpgradeProver") {
-    return { type: msg[0], prover: msg[1] };
-  } else if (msg[0] == "Removed") {
-    return { type: msg[0], prover: msg[1] };
-  } else {
-    return { type: msg[0] };
-  }
+    if (msg[0] == "Done") {
+        return { type: msg[0], result: msg[1] };
+    } else if (msg[0] == "Uninstalled") {
+        return { type: msg[0], prover: msg[1] };
+    } else if (msg[0] == "UpgradeProver") {
+        return { type: msg[0], prover: msg[1] };
+    } else if (msg[0] == "Removed") {
+        return { type: msg[0], prover: msg[1] };
+    } else {
+        return { type: msg[0] };
+    }
 }
 
 // Messages
